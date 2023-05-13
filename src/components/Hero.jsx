549cb381cfaf9a41f27c986cbef1c8379e9a576c
Diego sales <diegofelipesales23@gmail.com>
2023-05-13 04:34:27 -0400

    finalizando projeto MVP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />
+        Resuma artigos com <br className="max-md:hidden" />
         <span className="orange_gradient">OpenIA GPT-4</span>
       </h1>
       <h2 className="desc">
